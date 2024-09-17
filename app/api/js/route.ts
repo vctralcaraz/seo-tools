34cc7fa26883d28ca05ffc14f67a3df33edb0ca8
Victor Alcaraz <vctralcaraz@gmail.com>
2024-09-17 12:02:19 -0700

    better validations

--- app/api/js/route.ts
+++ app/api/js/route.ts
@@ -2,13 +2,18 @@ import { NextResponse } from "next/server";
 
 export async function GET(req: Request) {
   const { searchParams } = new URL(req.url);
-  const name = searchParams.get("name") || "null";
-  const scriptContent = `alert(${document.body});`;
+  const id = searchParams.get("id") || "null";
+  if (id) {
+    console.log("id has been added:", id);
+    const scriptContent = `alert(${document.body});`;
 
-  return new NextResponse(scriptContent, {
-    headers: {
-      "Content-Type": "application/javascript",
-      "Cache-Control": "no-store, max-age=0",
-    },
-  });
+    return new NextResponse(scriptContent, {
+      headers: {
+        "Content-Type": "application/javascript",
+        "Cache-Control": "no-store, max-age=0",
+      },
+    });
+  } else {
+    console.log("no id has been added");
+  }
 }
